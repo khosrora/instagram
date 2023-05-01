19f5e57e7c42e15182af64daa1336e75e6b329f1
khosro rasouli <khosrora@khosros-MacBook-Air.local>
2023-05-01 20:29:52 +0330

    add vector icons for ios and android

--- App.tsx
+++ App.tsx
@@ -9,6 +9,8 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { enableScreens } from 'react-native-screens';
 
+import Ionic from "react-native-vector-icons/Ionicons"
+import MIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 enableScreens();
 
@@ -17,6 +19,7 @@ function App(): JSX.Element {
   return (
     <View>
       <Text>Hello</Text>
+      <Ionic name="play" style={{ fontSize: 20 }} />
     </View>
   );
 }
